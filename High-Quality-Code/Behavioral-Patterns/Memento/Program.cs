b7f7b3f36beb69149c42995b56b041f2f4154e40
InKolev <in.kolev.b@gmail.com>
2015-09-23 15:06:49 +0300

    Restructured Memento Pattern code sample.

--- High-Quality-Code/Behavioral-Patterns/Memento/Program.cs
+++ High-Quality-Code/Behavioral-Patterns/Memento/Program.cs
@@ -17,30 +17,30 @@ namespace Memento
             var human = new HumanBody(174.00, 74.00, 8.00);
 
             statsMemory.Add(human.Save());
-            Console.WriteLine(human.ToString());
+            Console.WriteLine(String.Format("Initial State: {0}{1}", Environment.NewLine, human.ToString()));
 
             human.Eat();
             human.Grow();
             human.LoseFat();
 
             statsMemory.Add(human.Save());
-            Console.WriteLine(human.ToString());
+            Console.WriteLine(String.Format("First Change: {0}{1}", Environment.NewLine, human.ToString()));
 
             human.Eat();
             human.Grow();
             human.LoseFat();
 
             statsMemory.Add(human.Save());
-            Console.WriteLine(human.ToString());
+            Console.WriteLine(String.Format("Second Change: {0}{1}", Environment.NewLine, human.ToString()));
 
             human.Restore(statsMemory.Previous());
-            Console.WriteLine(human.ToString());
+            Console.WriteLine(String.Format("Restore Previous State: {0}{1}", Environment.NewLine, human.ToString()));
 
             human.Restore(statsMemory.Previous());
-            Console.WriteLine(human.ToString());
+            Console.WriteLine(String.Format("Restore Previous State: {0}{1}", Environment.NewLine, human.ToString()));
 
             human.Restore(statsMemory.Next());
-            Console.WriteLine(human.ToString());
+            Console.WriteLine(String.Format("Restore Next State: {0}{1}", Environment.NewLine, human.ToString()));
         }
     }
 }
